562bd8c37172caa9b25aace08bca7d073d50e9ef
johan villamil <johinsdev@gmail.com>
2019-07-15 09:21:49 -0500

    Update font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
-import { StyleSheet, Text, ActivityIndicator } from 'react-native'
+import { StyleSheet, ActivityIndicator } from 'react-native'
 import { View } from 'native-base'
+import { Font } from 'expo'
 
 import { cacheImages } from './src/utils/cacheImages'
 import images from './src/theme/images'
 import Navigation from './src/navigation'
 import { ListProvider } from './src/modules/posts/list/store/reducer'
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font))
+}
 
 export default function App() {
   const [isReady, setIsReady] = React.useState()
@@ -16,9 +21,12 @@ export default function App() {
 
   const cacheAssets = async () => {
     const imagesAssets = cacheImages([...Object.values(images)])
+    const fontAssets = cacheFonts([
+      { Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf') },
+    ])
 
     try {
-      await Promise.all([...imagesAssets])
+      await Promise.all([...imagesAssets, ...fontAssets])
 
       setIsReady(true)
     } catch (error) {
--- src/navigation.tsx
+++ src/navigation.tsx
@@ -2,6 +2,7 @@ import {
   createStackNavigator,
   createSwitchNavigator,
   createAppContainer,
+  createMaterialTopTabNavigator,
 } from 'react-navigation'
 
 const PostStack = createStackNavigator({
